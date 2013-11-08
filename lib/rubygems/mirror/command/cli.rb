2887c000e5e1c95e486f618348c4ecf71174ffff
Ahto Jussila <ahto@on-line.fi>
2013-11-08 19:03:43 +0200

    parallelise gemspec fetching too and actually use the parallelism number from yaml when downloading gems. Had to re-open and modify rubygems classes :/

--- lib/rubygems/mirror/command/cli.rb
+++ lib/rubygems/mirror/command/cli.rb
@@ -6,6 +6,65 @@ require 'thor'
 require "rubygems"
 require "rubygems/mirror/command"
 require "rubygems/mirror/fetcher"
+require "rubygems/mirror/pool"
+
+# we need to rewrite couple of methods because original doesn't give parallelism to Gem::Mirror.new
+# If original is changed we are screwed. Original is found on my machine in
+# ~/.rvm/gems/ruby-1.9.3-p448@rubygems-mirror-command/gems/rubygems-mirror-1.0.1/lib/rubygems/mirror/command.rb
+class Gem::Commands::MirrorCommand
+  attr_reader :parallelism
+
+  def initialize(parallelism = 10)
+    @parallelism = parallelism
+    super 'mirror', 'Mirror a gem repository'
+  end
+
+  def execute
+    config_file = File.join Gem.user_home, '.gem', '.mirrorrc'
+
+    raise "Config file #{config_file} not found" unless File.exist? config_file
+
+    mirrors = YAML.load_file config_file
+
+    raise "Invalid config file #{config_file}" unless mirrors.respond_to? :each
+
+    mirrors.each do |mir|
+      raise "mirror missing 'from' field" unless mir.has_key? 'from'
+      raise "mirror missing 'to' field" unless mir.has_key? 'to'
+
+      get_from = mir['from']
+      save_to = File.expand_path mir['to']
+
+      raise "Directory not found: #{save_to}" unless File.exist? save_to
+      raise "Not a directory: #{save_to}" unless File.directory? save_to
+
+      mirror = Gem::Mirror.new(get_from, save_to, parallelism)
+
+      say "Fetching: #{mirror.from(Gem::Mirror::SPECS_FILE_Z)}"
+      mirror.update_specs
+
+      say "Total gems: #{mirror.gems.size}"
+
+      num_to_fetch = mirror.gems_to_fetch.size
+
+      progress = ui.progress_reporter num_to_fetch,
+                                      "Fetching #{num_to_fetch} gems"
+
+      trap(:INFO) { puts "Fetched: #{progress.count}/#{num_to_fetch}" } if SUPPORTS_INFO_SIGNAL
+
+      mirror.update_gems { progress.updated true }
+
+      num_to_delete = mirror.gems_to_delete.size
+
+      progress = ui.progress_reporter num_to_delete,
+                                      "Deleting #{num_to_delete} gems"
+
+      trap(:INFO) { puts "Fetched: #{progress.count}/#{num_to_delete}" } if SUPPORTS_INFO_SIGNAL
+
+      mirror.delete_gems { progress.updated true }
+    end
+  end
+end
 
 module Rubygems
   module Mirror
@@ -29,9 +88,10 @@ module Rubygems
 
         desc "fetch", "fetch all the necessary files to the server."
         def fetch
-          fetch_allgems
-          fetch_basefiles
-          fetch_gemspecs
+          fetch_allgems(false)
+          fetch_basefiles(false)
+          fetch_gemspecs(false)
+          exit 0 #signal successful exit
         end
 
         desc "server [port]", "start mirror server."
@@ -40,33 +100,45 @@ module Rubygems
         end
 
         desc "fetch_allgems", "fetch only gems."
-        def fetch_allgems
+        def fetch_allgems(exit_successfully=true)
           config_file
           say "fetch_allgems start!", :GREEN
-          Gem::Commands::MirrorCommand.new.execute
+          Gem::Commands::MirrorCommand.new(parallelism).execute
           say "fetch_allgems end!", :GREEN
+          exit 0 if exit_successfully
         end
 
         desc "fetch_gemspecs", "fetch only gemspec files."
-        def fetch_gemspecs
+        def fetch_gemspecs(exit_successfully=true)
+          @pool = Gem::Mirror::Pool.new(parallelism)
+
           say "fetch gemspecs start!", :GREEN
           Dir::foreach(File.join(to,'gems')) do |filename|
-            next if filename == "." or filename == ".."
-            gem_name = File.basename filename, ".gem"
-            say " -> #{gem_name}.gemspec.rz", :BLUE
-            _fetch(File.join(GEMSPECS_DIR, "#{gem_name}.gemspec.rz"))
+
+            @pool.job do
+              next if filename == "." or filename == ".."
+              gem_name = File.basename filename, ".gem"
+              say " -> #{gem_name}.gemspec.rz", :BLUE
+              _fetch(File.join(GEMSPECS_DIR, "#{gem_name}.gemspec.rz"))
+            end
+
           end
+
+          @pool.run_til_done
+
           say "fetch gemspecs end!", :GREEN
+          exit 0 if exit_successfully
         end
 
         desc "fetch_basefiles", "fetch only base files."
-        def fetch_basefiles
+        def fetch_basefiles(exit_successfully=true)
           say "fetch basefiles start!", :GREEN
           BASE_FILES.each do |filename|
             say " -> #{filename}", :BLUE
             _fetch(filename)
           end
           say "fetch basefiles end!", :GREEN
+          exit 0 if exit_successfully
         end
 
         private
@@ -74,6 +146,12 @@ module Rubygems
         def _fetch(filename)
           @fetcher ||= Gem::Mirror::Fetcher.new
           @fetcher.fetch(File.join(from, filename), File.join(to, filename))
+        end
+
+        def parallelism
+          Integer(mirror['parallelism'])
+        rescue
+          10
         end
 
         def from
